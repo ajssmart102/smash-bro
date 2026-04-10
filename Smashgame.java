319c66788678384c23cd6f7ba7160e1e015c90f3
AntoUli31 <antonioulices9@gmail.com>
2026-04-10 08:23:17 -0500

    Update Smashgame.java

--- Smashgame.java
+++ Smashgame.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Smashgame {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            GameWindow window = new GameWindow();
+            Gamewindow window = new Gamewindow();
             window.setVisible(true);
             window.startGame();
         });
