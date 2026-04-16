04b2fe377c8b8117b36204cf1e311a8e62893ef0
AntoUli31 <antonioulices9@gmail.com>
2026-04-16 08:24:32 -0500

    Delete Smashgame.java

--- Smashgame.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
- 
-public class Smashgame {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new Gamewindow(); // Gamewindow now handles map select → game start
-        });
-    }
-}
